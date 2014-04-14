657e4be7d475ecc0729c2479f45102fcc8f383c2
nbsky <chpeiqi@gmail.com>
2014-04-14 16:06:24 +0800

    modified:   test.py 	modified:   weibo.html 	deleted:    account 	deleted:    login.py

--- loginWeibo/test.py
+++ loginWeibo/test.py
@@ -1,6 +1,6 @@
 from Weibo import *
 weibo = Weibo()
-weibo.Login('547010823@qq.com','60buaichiyu')
+weibo.Login('547010823@qq.com','')
 html = weibo.Fetch('http://weibo.cn/buaichiyu')
 file = open('weibo.html','w')
 file.write(html)
